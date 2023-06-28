646a1cc1c23891509145632fef298e25647d1d4a
antalika <kn@datasitch.com>
2023-06-28 16:34:09 +0300

    fix names

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,19 +9,19 @@ export default {
             //primary: colors["var(--link-color)"],
 
             primary: {
-                "extra-dark": "var(--color-primary-extra-dark)",
+                darker: "var(--color-primary-darker)",
                 dark: "var(--color-primary-dark)",
                 light: "var(--color-primary-light)",
-                "extra-light": "var(--color-primary-extra-light)",
-                "ultra-light": "var(--color-primary-ultra-light)",
+                lighter: "var(--color-primary-lighter)",
+                lightest: "var(--color-primary-lightest)",
                 DEFAULT: "var(--color-primary)",
             },
             secondary: {
-                "extra-dark": "var(--color-secondary-extra-dark)",
+                darker: "var(--color-secondary-darker)",
                 dark: "var(--color-secondary-dark)",
                 light: "var(--color-secondary-light)",
-                "extra-light": "var(--color-secondary-extra-light)",
-                "ultra-light": "var(--color-secondary-ultra-light)",
+                lighter: "var(--color-secondary-lighter)",
+                lightest: "var(--color-secondary-lightest)",
                 DEFAULT: "var(--color-secondary)",
             },
             ...colors
